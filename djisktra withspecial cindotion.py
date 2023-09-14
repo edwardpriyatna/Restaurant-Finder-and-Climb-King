37498f4a0e1f9c6f6575aeb36c0436d9ce0d668f
edwa0003 <edwa0003@student.monash.edu>
2023-09-14 08:49:54 +0800

    made basic djisktra's

--- djisktra withspecial cindotion.py
+++ djisktra withspecial cindotion.py
@@ -1,117 +1,88 @@
 import heapq
 
-
 class Vertex:
-    def __init__(self, id, weight=0):
-        self.id = id
-        self.weight = weight
-        self.edges = None
-        self.next = None
+    def __init__(self, name):
+        self.name = name
         self.visited = False
         self.discovered = False
+        self.time_to_reach = float('inf')
+        self.edges = []
+        self.previous_vertex = None  # Add this line
 
-    def add_edge(self, edge):
-        if not self.edges:
-            self.edges = edge
-        else:
-            current_edge = self.edges
-            while current_edge.next:
-                current_edge = current_edge.next
-            current_edge.next = edge
-
+    def __lt__(self, other):
+        return self.time_to_reach < other.time_to_reach
 
 class Edge:
-    def __init__(self, start_vertex, end_vertex, weight):
-        self.start_vertex = start_vertex
-        self.end_vertex = end_vertex
+    def __init__(self, to_vertex, weight):
+        self.to_vertex = to_vertex
         self.weight = weight
-        self.next = None
-
 
 class Graph:
-    def __init__(self, paths, keys):
-        self.vertices = Vertex(0)
-        current_vertex = self.vertices
-        for i in range(1, max(max(paths)) + 1):
-            current_vertex.next = Vertex(i)
-            current_vertex = current_vertex.next
+    def __init__(self):
+        self.vertices = []
 
-        for key in keys:
-            current_vertex = self.vertices
-            while current_vertex and current_vertex.id != key[0]:
-                current_vertex = current_vertex.next
-            if current_vertex:
-                current_vertex.weight = key[1]
-
-        for path in paths:
-            start_vertex = self.vertices
-            while start_vertex and start_vertex.id != path[0]:
-                start_vertex = start_vertex.next
-
-            end_vertex = self.vertices
-            while end_vertex and end_vertex.id != path[1]:
-                end_vertex = end_vertex.next
-
-            if start_vertex and end_vertex:
-                edge = Edge(start_vertex, end_vertex, path[2])
-                start_vertex.add_edge(edge)
+    def add_vertex(self, vertex):
+        self.vertices.append(vertex)
 
 
-def modified_dijkstra(graph, start_id, end_id):
-    queue = []
-    heapq.heappush(queue, (0, start_id, [], 0))
+    def dijkstra(self, start_vertex):
+        queue = []
+        start_vertex.time_to_reach = 0
+        heapq.heappush(queue, start_vertex)
 
-    start_node = graph.vertices
-    while start_node and start_node.id != start_id:
-        start_node = start_node.next
+        while queue:
+            current_vertex = heapq.heappop(queue)
+            current_vertex.visited = True
 
-    if start_node:
-        start_node.discovered = True
+            for edge in current_vertex.edges:
+                if not edge.to_vertex.visited:
+                    tentative_distance = current_vertex.time_to_reach + edge.weight
+                    if tentative_distance < edge.to_vertex.time_to_reach:
+                        edge.to_vertex.time_to_reach = tentative_distance
+                        edge.to_vertex.discovered = True
+                        edge.to_vertex.previous_vertex = current_vertex
+                        heapq.heappush(queue, edge.to_vertex)
 
-    while queue:
-        (cost, node_id, path, added_weight) = heapq.heappop(queue)
+    def get_shortest_path(self, start_vertex, end_vertex):
+        self.dijkstra(start_vertex)
+        path = []
+        current_vertex = end_vertex
 
-        node_weight = 0
-        node = graph.vertices
-        while node and node.id != node_id:
-            node = node.next
+        while current_vertex is not None:
+            path.append(current_vertex.name)
+            current_vertex = current_vertex.previous_vertex
 
-        if node and not node.visited:
-            node.visited = True
+        path.reverse()
+        return end_vertex.time_to_reach, path
 
-            path = path + [node_id]
+if __name__ == "__main__":
+    # Create vertices
+    A = Vertex('A')
+    B = Vertex('B')
+    C = Vertex('C')
+    D = Vertex('D')
+    E = Vertex('E')
 
-            if node.weight > 0 and added_weight == 0:
-                node_weight = node.weight
-                added_weight += node_weight
+    # Create edges
+    A.edges.append(Edge(B, 0))
+    B.edges.append(Edge(A, 1))
+    A.edges.append(Edge(C, 3))
+    C.edges.append(Edge(A, 2))
+    B.edges.append(Edge(D, 1))
 
-            if node_id == end_id and added_weight > 0:
-                return cost + added_weight, path
-
-            edge = None
-            if node:
-                edge = node.edges
-
-            while edge:
-                total_cost = cost + edge.weight + node_weight
-
-                next_node = graph.vertices
-                while next_node and next_node.id != edge.end_vertex.id:
-                    next_node = next_node.next
-
-                if next_node and not next_node.discovered:
-                    next_node.discovered = True
-
-                    heapq.heappush(queue, (total_cost, edge.end_vertex.id, path, added_weight))
-
-                edge = edge.next
-
-    return float("inf"), []
+    C.edges.append(Edge(E, 4))
+    E.edges.append(Edge(C, 1))
+    D.edges.append(Edge(C, 1))
 
 
-# Define your graph here. For example:
-paths = [(0, 1, 4), (0, 3, 2), (0, 2, 3), (2, 3, 2), (3, 0, 3)]
-keys = [(0, 5), (3, 2), (1, 3)]
-graph = Graph(paths, keys)
+    # Create graph and add vertices
+    graph = Graph()
+    graph.add_vertex(A)
+    graph.add_vertex(B)
+    graph.add_vertex(C)
+    graph.add_vertex(D)
+    graph.add_vertex(E)
 
-print(modified_dijkstra(graph, 0, 3))
+    # Get shortest path from A to E
+    distance, path = graph.get_shortest_path(B, E)
+    print(f"The shortest path from A to E is {path} with a total weight of {distance}")
